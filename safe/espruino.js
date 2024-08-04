a46b9a44d63f9e595b118e357078aa015c5513ee
Espen Klem <espen.klem@gmail.com>
2024-08-04 15:32:17 +0200

    Added LED communication booted, ready for input, correct code, wrong code, unlocked, locked

--- safe/espruino.js
+++ safe/espruino.js
@@ -8,8 +8,8 @@ var salt = 'sad88hfoiwhe38405';
 var crypto = require("crypto");
 
 
-// Different led messages via LED
-// 1: Ready - 3 medium blink
+// Different led messages via LED - red and green
+// 1: Booted and ready - long green blink
 // 2: Key input - 1 short blink
 // 3: Correct code - 1 long blink
 // 4: Locking - 1 long blink
@@ -28,11 +28,13 @@ function onKey(key) {
   var character = '123456789*0#'[key];
   console.log('key: ' + character);
   if (character === '*' && open === true) {
+    message('locking');
     s.move(1);
     console.log('*** locking safe ***');
     open = false;
   } else if (character === '*' && codeStarted === false && open === false) {
     // start register code
+    message('ready');
     codeStarted = true;
     console.log('ready for code');
   } else if (character === '*' && codeStarted === true) {
@@ -46,18 +48,49 @@ function onKey(key) {
       s.move(0);
       open = true;
       console.log('*** correct code, opening safe ****');
+      message('codeCorrect');
       resetCodeInput();
     } else {
       console.log('*** wrong code, try again ****');
+      message('codeWrong');
       resetCodeInput();
     }
   } else if (codeStarted === true ) {
+    message('codeInput');
     codeTyped += character;
     console.log('codeTyped: ' + codeTyped);
     // check against our code
   }
 }
 
+function message(type) {
+  // LED info messages - two LEDs red & green
+  const green = 'B7';
+  const red = 'A8';
+  if (type === 'booted') {
+    digitalPulse(green,1,[1000,0]);
+  }
+  if (type === 'ready') {
+    digitalPulse(green,1,[100,100,100,100,100]);
+  }
+  if (type === 'codeInput') {
+    digitalPulse(green,1,[50,0]);
+  }
+  if (type === 'codeCorrect') {
+    digitalPulse(green,1,[1000,0]);
+  }
+  if (type === 'codeWrong') {
+    digitalPulse(red,1,[100,100,100,100,100,100,100,100,100,100,100]);
+  }
+  if (type === 'locking') {
+    digitalPulse(green,1,[1000,0]);
+    digitalPulse(red,1,[1000,0]);
+  }
+}
+
+message('booted');
+
 require("KeyPad").connect([B15,B14,B13],[B10,B1,A7,A6], function(key) {
   onKey(key);
 });
+  
