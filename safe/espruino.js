2424f7788264f462463d72bb7025863f57b9f3e5
Espen Klem <espen.klem@gmail.com>
2024-08-04 20:43:58 +0200

    Cleaning up code a bit * Swapping from message() to blink() so it's less code guessing. * removing old blink message comments

--- safe/espruino.js
+++ safe/espruino.js
@@ -7,15 +7,6 @@ var passphraseEncrypted = '1122287887126168932401488510822216238195157';
 var salt = 'sad88hfoiwhe38405';
 var crypto = require("crypto");
 
-
-// Different led messages via LED - red and green
-// 1: Booted and ready - long green blink
-// 2: Key input - 1 short blink
-// 3: Correct code - 1 long blink
-// 4: Locking - 1 long blink
-// Or 3 LEDs - one for error and two for input feedback
-// 1: Show that it takes input - LED2 is lighting
-
 function resetCodeInput() {
   codeTyped = [''];
   codeStarted = false;
@@ -28,13 +19,13 @@ function onKey(key) {
   var character = '123456789*0#'[key];
   console.log('key: ' + character);
   if (character === '*' && open === true) {
-    message('locking');
+    blink('locking');
     s.move(1);
     console.log('*** locking safe ***');
     open = false;
   } else if (character === '*' && codeStarted === false && open === false) {
     // start register code
-    message('ready');
+    blink('ready');
     codeStarted = true;
     console.log('ready for code');
   } else if (character === '*' && codeStarted === true) {
@@ -48,23 +39,23 @@ function onKey(key) {
       s.move(0);
       open = true;
       console.log('*** correct code, opening safe ****');
-      message('codeCorrect');
+      blink('codeCorrect');
       resetCodeInput();
     } else {
       console.log('*** wrong code, try again ****');
-      message('codeWrong');
+      blink('codeWrong');
       resetCodeInput();
     }
   } else if (codeStarted === true ) {
-    message('codeInput');
+    blink('codeInput');
     codeTyped += character;
     console.log('codeTyped: ' + codeTyped);
     // check against our code
   }
 }
 
-function message(type) {
-  // LED info messages - two LEDs red & green
+function blink(type) {
+  // LED info blink message - two LEDs red & green
   const green = 'B7';
   const red = 'A8';
   if (type === 'booted') {
@@ -88,7 +79,7 @@ function message(type) {
   }
 }
 
-message('booted');
+blink('booted');
 //locking if not locked
 s.move(1);
 
